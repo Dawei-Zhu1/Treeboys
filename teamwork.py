e971214cfb4f1a107fd8cff2f82616fa5313ce96
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2024-02-20 20:26:28 +1000

    1. Update the square_numbers function 2. Format the script

--- teamwork.py
+++ teamwork.py
@@ -1,11 +1,14 @@
 def get_numbers():
-    numbers_input = input(" Enter numbers separeated by commas: ")
+    numbers_input = input("Enter numbers separated by commas: ")
     number = [float(num) for num in numbers_input.split(",")]
     return number
 
+
 def square_numbers(numbers):
     for i in range(len(numbers)):
-        numbers[i] = numbers[i] **2
+        numbers[i] = numbers[i] ** 2
+
+
 def display_numbers(numbers):
     for num in numbers:
         print(num, end=".")
@@ -16,5 +19,10 @@ def main():
     numbers = get_numbers()
     square_numbers(numbers)
     display_numbers(numbers)
+
+
+def square_numbers(numbers):
+    return [number ** 2 for number in numbers]
+
 
 main()
