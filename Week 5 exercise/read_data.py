6591594dc72b3ad63557e5002c1a4d896922e5eb
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2024-02-27 19:14:23 +1000

    Add exercise for week 5

---
+++ Week 5 exercise/read_data.py
@@ -0,0 +1,14 @@
+"""
+Prac 05: Read data (group work)
+"""
+
+
+def main():
+    with open('countries.csv', 'r') as data:
+        # Code for processing the data
+        # 1. Load countries.csv and saving in proper format
+        # 2. Display the data format and sorted
+        pass
+
+
+main()
